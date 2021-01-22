5c2bb43f598c042e9ac4903e5102130bb2ec7632
NastiaZe <anastasiazemskova4212gmail.com>
2021-01-22 21:59:49 +0300

    commit

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,66 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+
+    int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+    @Test
+    void shouldCalculateSum() {
+
+        StatsService service = new StatsService();
+
+        int expected = 180;
+        int actual = (int) service.calculateSum(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateAverage() {
+        StatsService service = new StatsService();
+
+        int expected = 15;
+        int actual = service.calculateAverage(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateMax() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.findMax(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateMin() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.findMin(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculatelessThanAverage() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.lessThanAverage(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculatemoreThanAverage() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.moreThanAverage(sales);
+        assertEquals(expected, actual);
+    }
+}
